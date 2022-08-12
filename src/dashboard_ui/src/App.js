a007b46ef669aba18425e862eb93b28d2fc68ee9
abdulquadir01 <abdulquadir01@outlook.com>
2022-08-12 05:48:40 +0530

    added MatchPage

--- src/dashboard_ui/src/App.js
+++ src/dashboard_ui/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 
 import {Route, Routes} from 'react-router-dom';
-import TeamPage from './pages/TeamPage';
+// import { Switch } from 'react-router-dom';
+import {TeamPage} from './pages/TeamPage';
+import {MatchPage} from './pages/MatchPage';
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/teams/:teamName" element={<TeamPage/>}> 
-            {/* <TeamPage /> */}
-          </Route>
+        {/* <Switch> */}
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage MatchPage />} /> 
+          <Route path="/teams/:teamName" element={<TeamPage />} /> 
+        {/* </Switch> */}
       </Routes>
     </div>
   );
---
+++ src/dashboard_ui/src/pages/MatchPage.js
@@ -0,0 +1,12 @@
+import { React } from 'react';
+
+
+export const MatchPage = () => {
+
+
+    return (
+        <div className="MatchPage">
+            <h2>Match Detail Page</h2>
+        </div>
+    );
+}
