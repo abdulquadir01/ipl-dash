75ee22b5c7973da3b7c4eb1af6fc8290e94bc3bf
abdulquadir01 <abdulquadir01@outlook.com>
2022-08-13 07:34:01 +0530

    added match api

--- src/main/java/org/springlearner/ipldashboard/controllers/TeamController.java
+++ src/main/java/org/springlearner/ipldashboard/controllers/TeamController.java
@@ -1,10 +1,15 @@
 package org.springlearner.ipldashboard.controllers;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springlearner.ipldashboard.models.Team;
+import org.springlearner.ipldashboard.models.Match;
 import org.springlearner.ipldashboard.repositories.MatchRepository;
 import org.springlearner.ipldashboard.repositories.TeamRepository;
 
@@ -30,4 +35,13 @@ public class TeamController {
         return team;
     }
 
+    @GetMapping("/teams/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+        
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year+1, 1, 1);
+
+        return matchRepository.getMatchesByTeamBetweenDates( teamName, startDate, endDate );
+        
+    }
 }
